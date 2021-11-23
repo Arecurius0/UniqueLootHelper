043486303e0bb5d46d9fe68f74a13ffa08147f42
Arecurius <marcel@kirsti.de>
2021-11-23 15:23:01 +0100

    Add project files.

---
+++ Settings.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace UniqueLootHelper
+{
+    public class Settings : ISettings
+    {
+        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+        public ButtonNode RefreshUniquesFile { get; set; } = new ButtonNode();
+        public ColorNode Color { get; set; } = new ColorNode(SharpDX.Color.Purple);
+        public RangeNode<int> FrameThickness { get; set; } = new RangeNode<int>(2, 1, 5);
+    }
+}
---
+++ UniqueLootHelperCore.cs
@@ -0,0 +1,70 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueLootHelper
+{
+    public class UniqueLootHelperCore : BaseSettingsPlugin<Settings>
+    {
+        private const string UNIQUESARTWORK_FILE = "UniquesArtworks.txt";
+        private HashSet<String> UniquesHashSet;
+
+        public override bool Initialise()
+        {
+            Name = "UniqueLootHelper";
+            Settings.RefreshUniquesFile.OnPressed += () => { ReadUniquesArtworkFile(); };
+            ReadUniquesArtworkFile();
+            return base.Initialise();
+        }
+        private void ReadUniquesArtworkFile()
+        {
+            var path = $"{DirectoryFullName}\\{UNIQUESARTWORK_FILE}";
+            if (File.Exists(path))
+            {
+                UniquesHashSet = File.ReadAllLines(path).Where(line => !string.IsNullOrWhiteSpace(line) && !line.StartsWith("#")).ToHashSet();
+            }
+            else
+                CreateUniquesArtworkFile();
+        }
+
+        private void CreateUniquesArtworkFile()
+        {
+            var path = $"{DirectoryFullName}\\{UNIQUESARTWORK_FILE}";
+            if (File.Exists(path)) return;
+            using (var streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.Write("");
+                streamWriter.Close();
+            }
+        }
+        public override void Render()
+        {
+            if(GameController.Area.CurrentArea.IsHideout ||
+                GameController.Area.CurrentArea.IsTown)
+            {
+                return;
+            }
+            
+            foreach(var label in GameController.IngameState.IngameUi.ItemsOnGroundLabelsVisible)
+            {
+                if(UniquesHashSet.Contains(label.ItemOnGround.GetComponent<WorldItem>()?.ItemEntity.GetComponent<RenderItem>()?.ResourcePath))
+                {
+                    Graphics.DrawFrame(label.Label.GetClientRectCache, Settings.Color, Settings.FrameThickness);
+                }
+            }
+            
+            base.Render();
+        }
+
+        public override Job Tick()
+        {
+
+            return base.Tick();
+        }
+    }
+}
